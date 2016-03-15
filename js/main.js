69eb90825628d81a803ba86b9d617575cd879446
Nicolas Thiry <n.thiry92@gmail.com>
2016-03-15 20:36:35 +0100

    removed picture animation

--- js/main.js
+++ js/main.js
@@ -29,11 +29,8 @@ window.requestAnimationFrame = window.requestAnimationFrame
 
 function parallaxbubbles(){
 	var resume = document.getElementById('n-resume-frame')
-	var picture = document.getElementById('n-picture')
  	var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
  	resume.style.marginTop = 65 - scrolltop * .1 + 'px'
-	picture.style.left = -220 - scrolltop * .1 + 'px'
-	picture.style.top = 125 - scrolltop * .1 + 'px'
 }
 
 window.addEventListener('scroll', function(){ // on page scroll
